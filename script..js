8f461b64605891bd167e11126e15fb049eb926b2
sofffja <sofi.efron@gmail.com>
2023-12-19 09:30:18 -0300

    Add rainbow mode

--- script..js
+++ script..js
@@ -1,11 +1,28 @@
 const gridSize = 512;
 let gridResolution = 16;
+let colorMode = 'black';
+let ereaser = false;
 
 const container = document.querySelector('.container');
 container.style.maxWidth = `${gridSize}px`;
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
+makeGrid();
+
+container.addEventListener('mouseover', (e) => {
+  if (e.buttons === 1) {
+    if (colorMode === 'black') {
+      e.target.style.backgroundColor = 'black';
+    } else if (colorMode === 'color') {
+      let randomR = Math.random() * 255;
+      let randomG = Math.random() * 255;
+      let randomB = Math.random() * 255;
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    }
+  }
+})
+
+const gridBtn = document.querySelector('.new-grid');
+gridBtn.addEventListener('click', () => {
   gridResolution = prompt('select grid resolution (max 100)', '');
   if (gridResolution > 100) {
     gridResolution = prompt('please enter a number smaller than 100', '');
@@ -13,8 +30,20 @@ btn.addEventListener('click', () => {
   makeGrid();
 })
 
-container.addEventListener('mouseover', (e) => {
-  e.target.classList.add('hovered');
+const colorBtn = document.querySelector('.change-color')
+colorBtn.addEventListener('click', () => {
+  switch (colorMode) {
+    case 'black':
+      colorMode = 'color';
+      colorBtn.textContent = 'black mode';
+      ereaser = false;
+      break;
+    case 'color':
+      colorMode = 'black';
+      colorBtn.textContent = 'rainbow mode';
+      ereaser = false;
+      break;
+  }
 })
 
 function makeGrid() {
